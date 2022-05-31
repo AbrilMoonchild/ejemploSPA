c844b07e11135eaf98de574b4b592425d0748cdc
Angela Alarcon <aangelaabril101@gmail.com>
2022-05-30 20:54:20 -0400

    first commit

---
+++ src/routes/Navigation.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route,NavLink, Navigate} from 'react-router-dom'
+import {Home} from '../components/Home'
+import {About} from '../components/About'
+import {Students} from '../components/Students'
+import logo from '../logo.svg'
+function Navigation() {
+  return (
+      
+      <BrowserRouter>
+      <nav>
+          <img src={logo} alt="logo"></img>
+        <ul>
+             <li>
+                <NavLink to='/' className={({isActive}) => isActive ? 'nav-active' : ''}>
+                     Home
+                </NavLink>    
+             </li>
+             <li>
+                <NavLink to='/about' className={({isActive}) => isActive ? 'nav-active' : ''}>
+                     About
+                </NavLink>
+             </li>
+             <li>
+                <NavLink to='/students' className={({isActive}) => isActive ? 'nav-active' : ''}>
+                    Students
+                </NavLink></li> 
+        </ul>
+      </nav>
+        <Routes>
+            
+            <Route path='about' element={<About></About>}></Route>
+            <Route path='students' element={<Students></Students>}></Route>
+            <Route path='home' element={<Home></Home>}></Route>
+            <Route path='/*' element={<Navigate to='/home' replace></Navigate>}></Route>
+        </Routes>
+      </BrowserRouter>
+    
+  )
+}
+
+export {Navigation} 
